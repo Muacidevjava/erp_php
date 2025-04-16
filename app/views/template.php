106ca2758e3e85fc3e9240304477e29903e2f493
Muacidevjava <muaci@yahoo.com>
2025-04-16 07:17:06 -0300

    fatiando layout

--- app/views/template.php
+++ app/views/template.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-<meta charset="utf-8">
-<title>mjailton.com.br</title>
-<link rel="shortcut icon" href="" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
 
-			<?php include "cabecalho.php" ?>      
-            <?php $this->load($view, $viewData); ?>         
-           	<?php include "rodape.php" ?>	
-    </body>
+<head>
+    <title>Sistema ERP - mjailton</title>
+    <link rel="shortcut icon" href="" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale =1">
+    <!--css-->
+    <link rel="stylesheet" href="<?php echo URL_BASE ?>assets/css/auxiliar.css">
+    <link rel="stylesheet" href="<?php echo URL_BASE ?>assets/css/grade.css">
+    <link rel="stylesheet" href="<?php echo URL_BASE ?>assets/css/style.css">
+</head>
+
+<body>
+    <?php include "cabecalho.php"; ?>
+    <?php include "menu.php";?>
+    <div class="rows mx-0">
+        <div class="col-9 central mb-3">
+     <?php $this->load($view, $viewData)?>     
+        </div>
+
+    </div>
+
+    <!--font icones-->
+    <script src="<?php echo URL_BASE ?>assets/js/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://kit.fontawesome.com/9480317a2f.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/js.js"></script>
+
+    <!-- Graphs -->
+    <script src="<?php echo URL_BASE ?>assets/js/chart.js/Chart.min.js"></script>
+
+</body>
+
 </html>
